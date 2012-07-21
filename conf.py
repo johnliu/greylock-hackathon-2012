ee6ec66c1d8df47a03e1bf6373a8d24414bb3a6e
John Liu <johnyuhanliu@gmail.com>
2012-07-21 16:06:48 -0700

    Environment and configs.

--- conf.py
+++ conf.py
@@ -9,4 +9,7 @@ class Config(object):
   SECRET_KEY = unicode(os.environ.get('SECRET'))
 
   FIREBASE = os.environ.get('FIREBASE_URL')
+  GS_SECRET = os.environ.get('GS_SECRET')
+  GS_KEY = os.environ.get('GS_KEY')
+  GS_TINYSONG = os.environ.get('GS_TINYSONG')
 
