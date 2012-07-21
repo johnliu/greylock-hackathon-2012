0cd20027cc89dbc962ae3ca663f0bbdc70335013
John Liu <johnyuhanliu@gmail.com>
2012-07-21 14:23:26 -0700

    Adding firebase.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_object('conf.Config')
 
+
+def render_base(template='base.html', **kwargs):
+  default_args = {
+    'firebase_url': app.config['FIREBASE']
+  }
+
+  merged_args = dict(default_args, **kwargs)
+  return render_template(template, **merged_args)
+
+
 @app.route('/')
 def front():
-  """
-  The front page.
-  """
-  return "Hello world!"
+  return render_base()
 
 
 if __name__ == '__main__':
