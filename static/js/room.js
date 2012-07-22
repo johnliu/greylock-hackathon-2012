9ba044a1a50fdf57df8135b9193d0ed481ae0d9a
Tim Cheng <timcheng1006@gmail.com>
2012-07-22 03:43:48 -0700

    Fixed the js handle for toggling nav-pills

--- static/js/room.js
+++ static/js/room.js
@@ -1,1 +1,18 @@
 $('.queue a.song-link').tooltip({delay:{show:500, hide:10}, placement:'top'})
+
+$('#search-box').keypress(function (e) {
+  if (e.keyCode == 13) {
+    $('.nav-pills li').removeClass('active');
+    $('.content-box .tab-pane').removeClass('active');
+    $('#search-result-tab').addClass('active');
+    $('#search-result-content').addClass('active');
+  }
+});
+
+$('.nav-pills li a').click(function (e) {
+  $('.nav-pills li').removeClass('active');
+  $('.content-box .tab-pane').removeClass('active');
+  $(this).addClass('active');
+  $(this.getAttribute('href')).addClass('active');
+  e.preventDefault();
+});
