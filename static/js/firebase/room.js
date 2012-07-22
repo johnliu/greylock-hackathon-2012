253a60a6cbe3956e3167991b2d020b503d48b0b8
Roxanne Guo <roxane.guo@gmail.com>
2012-07-22 02:13:58 -0700

    TODO guest list update

--- static/js/firebase/room.js
+++ static/js/firebase/room.js
@@ -1,7 +1,20 @@
 // add new guest for each page landing
 
-$(document).ready(function() {
-  var room = window.location.pathname;
-  //db.child('rooms').child(room).child('guests').child(guest_name
+$(document).ready(function() { 
+  // create user
+  var room = window.location.pathname.split('/')[1];
+  var guests = ['guest_' + Math.round(Math.random() * 1000)];
+  db.child('rooms').child(room).child('guests').set(guests);
+
+  db.child('rooms').child(room).on('child_added', function(snapshot) {
+    //if (snapshot.val().guests === null) guests = [];
+    //else guests = new Array(snapshot.val().guests);
+    console.log(guests);
+    guests.concat(snapshot.val().guests);
+    console.log(guests);
+  });
+
+  guests.push(name);
+  db.child('rooms').child(room).child('guests').set(name);
 })
 
