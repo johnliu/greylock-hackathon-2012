871413af5d5cccc1b25be75154473925d1497222
Roxanne Guo <roxane.guo@gmail.com>
2012-07-22 05:57:36 -0700

    update javascript dynamic content

--- static/js/app/search.js
+++ static/js/app/search.js
@@ -21,12 +21,17 @@ $(document).ready(function() {
       var query_set = $.parseJSON(data);
       $.each(query_set, function(i, obj) {
         var song = $('<tr />');
-        $("<td />", {text: obj.AlbumName}).appendTo(song);
-        $("<td />", {text: obj.ArtistName}).appendTo(song);
         $("<td />", {text: obj.SongName}).appendTo(song);
-        var link = "<a href='#' class='btn btn-success add-to-queue'>Add</a>";
+        $("<td />", {text: obj.ArtistName}).appendTo(song);
+        $("<td />", {text: obj.AlbumName}).appendTo(song);
+        var div = $("<div />", {class: 'action-buttons'});
+        var first_link = "<a href='#' class='btn btn-primary btn-mini add-to-queue'><i class='icon-plus icon-white'></i></a>";
+        div.append(first_link);
+        var second_link = "<a href='#' class='btn btn-warning btn-mini add-to-queue'><i class='icon-star icon-white'></i></a>";
+        div.append(second_link);
         var add_link = $("<td />");
-        add_link.append(link);
+        add_link.append(div);
+
         add_link.appendTo(song);
         song.appendTo(song_list);
       });
