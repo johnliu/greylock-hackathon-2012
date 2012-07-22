c85ed3f4e0fde9681f4ae9a092451955e99ca8b3
Roxanne Guo <roxane.guo@gmail.com>
2012-07-22 05:02:44 -0700

    add to playlist

--- static/js/app/room.js
+++ static/js/app/room.js
@@ -15,11 +15,16 @@ $(document).ready(function() {
   guests.push(name);
   db.child('rooms').child(room).child('guests').set(name);
 
-  // song search field
   var room = window.location.pathname;
 
+  // push songs to the playlist
+  $('.add-to-queue').click(function() {
+  });
+
+  // song search field
   var search_handler = function() {
     var search = $("input#search-box").val();
+    $('.search-result-text').find('span').text(search);
     var song_list = $('#song-list');
     song_list.empty();
     $.get('_search', {'search_query': search}, function(data) {
@@ -29,6 +34,10 @@ $(document).ready(function() {
         $("<td />", {text: obj.AlbumName}).appendTo(song);
         $("<td />", {text: obj.ArtistName}).appendTo(song);
         $("<td />", {text: obj.SongName}).appendTo(song);
+        var link = "<a href='#' class='btn btn-success add-to-queue'>Add</a>";
+        var add_link = $("<td />");
+        add_link.append(link);
+        add_link.appendTo(song);
         song.appendTo(song_list);
       });
     });
